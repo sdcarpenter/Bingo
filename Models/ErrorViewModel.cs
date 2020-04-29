9e579680e9de9aa70315de188802792a6bb3c668
Scott Carpenter <sdcarpenter@gmail.com>
2020-04-29 16:03:00 -0600

    Inital commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bingo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
